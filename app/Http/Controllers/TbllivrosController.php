64842d0fadbd6b1dc2f5ffdf6876508ff07c1741
marcs13013 <marcus.elias@etec.sp.gov.br>
2024-08-09 22:56:33 -0300

    First

---
+++ app/Http/Controllers/TbllivrosController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Http\Support\Facades\Validator;
+
+use App\Models\tbLivros;
+
+class TbllivrosController extends Controller
+{
+    //construir o crud.
+    
+    //Mostrar todos os registros da tabela livros
+    //Crud -> Read(leitura) Select/Visualizar
+
+    public function index(){
+        $regBook = tbllivros::All();
+        $contador = $regBook->count();
+
+        return 'Livros: ' . $contador . $regBook . Response()->json([],Response::HTTP_NO_CONTENT);
+    }
+
+    //Mostrar um tipo de registro especifico
+    //Crud -> Read(leitura) Select/Visualizar
+
+    //Função show busca o id, verifica se existe elementos dentro da variável e mostra os resultados encontrados e não encontrados 
+    public function show(string $id){ 
+        $regBook = tbllivros::find($id);
+
+        if ($regBook) {
+            return 'Livros Localizados: ' . $regBook . Response()->json([],Response::HTTP_NO_CONTENT);
+        }else{
+            return 'Livros Não Localizados: ' . Response()->json([],Response::HTTP_NO_CONTENT);
+        }
+    }
+
+    //Cadastrar registros
+    //Crud -> Create(criar/cadastrar)
+    public function store(Request $request){
+        $regBook = $request->All();
+
+        $regVerify = Validator::make($regBook,[
+            'nomeLivro'=>'required',
+            'generoLivro'=>'required',
+            'anoLivro'=>'required'
+        ]);
+
+        if($regVerify->fails()) {
+            return 'Registros Inválidos: ' . Response()->json([],Response::HTTP_NO_CONTENT);
+
+        }
+        $regBookCad = tbllivros::create($regBook);
+
+        if ($regBookCad) {
+            return 'Livros Cadastrados: ' . Response()->json([],Response::HTTP_NO_CONTENT);
+        }else{
+            return 'Livros Não Cadastrados: ' . Response()->json([],Response::HTTP_NO_CONTENT);
+
+        }
+
+    }
+
+    //Alterar registros
+    //Crud -> update(alterar)
+    public function update(){
+
+    }
+
+    //Deletar os registros
+    //Crud -> delete(apagar)
+    public function destroy(){
+
+    }
+
+    //Crud
+    //C reate
+    //r ead
+    //u pdate
+    //d elete
+
+}
